874fa574433d85de040e52417c11642193aac049
Tamas Piros <me@tamas.io>
2014-01-21 13:50:36 +0000

    fix

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express')
 
 app.configure(function() {
   app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+  app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP);
   app.use(express.bodyParser());
   app.use(app.router);
 });
@@ -19,6 +20,6 @@ app.get('/', api.all);
 app.get('/airport/:airporttype', api.airportType);
 app.get('/airport/iata/:iata', api.airportIATA);
 app.get('/airport/distance/:distance', api.airportDistance);
-server.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+server.listen(app.get('port'), app.get('ipaddr'), function(){
+	console.log('Express server listening on  IP: 'app.get('ipaddr') + ' and port ' + app.get('port'));
 });
