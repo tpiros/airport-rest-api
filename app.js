52708e00b297b06c94f0383add8d3bc32a2425d0
Tamas Piros <me@tamas.io>
2014-02-20 18:01:50 +0000

    openshift

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var express = require('express')
 
 app.configure(function() {
   app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
-  app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP);
+  app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
   app.use(express.bodyParser());
   app.use(app.router);
 });
